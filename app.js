95f1b36bbe75bab8e47df72f4c4c8099c7e48c4c
unknown <hafizhammadali08121422@gmail.com>
2023-10-03 21:53:35 +0500

    Javascript 1st Assignment

---
+++ app.js
@@ -0,0 +1,19 @@
+var error = "Error! Please enter a valid password."
+
+
+        alert(error)
+
+        var welcome = "Welcome to JS Land...\nHappy Coding!"
+
+        alert(welcome)
+
+        var js = "Welcome to JS Land..."
+
+        alert(js)
+
+
+        var hellow = "Hello... I can run JS through my web browser's console."
+
+        alert(hellow)
+
+        console.log(hellow);
